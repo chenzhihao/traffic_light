95e200fc3a8507ba246d117b16f81ce1afb1b58c
chenzhihao <cabtravis@gmail.com>
2017-03-04 00:07:35 +0800

    run simulates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import Light from './Light';
+import {COLORS} from './constants';
+import Timer from './Timer';
+import LightController from './LightController';
 
-const light = new Light();
-light.onColorChange(function (color) {
-  console.log(color);
-  console.log(this);
-});
+let startTimeStr = process.argv[2] || '00:00:00';
+let durationInSec = process.argv[3] || 1800;
 
-light.turnYellow();
+const northSouthLight = new Light({initialColor: COLORS.RED});
+const eastWestLight = new Light({initialColor: COLORS.GREEN});
+
+const northSouthTimer = new Timer({initialTimeStr: startTimeStr});
+const eastWestTimer = new Timer({initialTimeStr: startTimeStr});
+
+const northSouthLightController = new LightController({light: northSouthLight, timer: northSouthTimer, durationInSec});
+const eastWestLightController = new LightController({light: eastWestLight, timer: eastWestTimer, durationInSec});
+
+Promise.all([northSouthLightController.loop(), eastWestLightController.loop()])
+  .then(function () {
+    console.log('North-South Light:');
+    northSouthLightController.logs.forEach(log => console.log(log));
+    console.log('================');
+    console.log('East-West Light:');
+    northSouthLightController.logs.forEach(log => console.log(log));
+
+  })
+  .catch(err => console.log(err));
+
